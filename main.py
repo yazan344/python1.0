6d520854e883e44d41158b245df5469cf35855d7
yazan344 <106996982+yazan344@users.noreply.github.com>
2022-07-19 19:47:53 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+days=int(input("Enter days :"))*3600*24
+hours=int(input("Enter hours :"))*3600
+minutes=int(input("Enter minutes :"))*60
+seconds=int(input("Enter seconds :"))
+time=days+hours*minutes+seconds
+print("rhe total secounds is :",time)
